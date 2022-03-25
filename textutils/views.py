4607c0d1953815135b7d389f633d385bbb03afe7
HemangiShirsath1999 <nikamhemangi1999@gmail.com>
2022-03-25 16:44:11 +0530

    video7

--- textutils/views.py
+++ textutils/views.py
@@ -1,8 +1,14 @@
 # I have created this file-harryhttps://github.com/Hemangishirsath/protuple_django.git
 from django.http import HttpResponse
+
+
 def index(reuest):
-    return  HttpResponse("hello")
+    return HttpResponse("hello")
 
 
 def about(request):
     return HttpResponse("bay")
+
+
+def index(request):
+    return HttpResponse('''<h1>hemangi</h1><a href="https://www.wesocioo.com/">hii</a> ''')
