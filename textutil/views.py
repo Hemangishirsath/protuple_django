8f39f0a80a8ebf6efb4e60576a591901c449b57b
HemangiShirsath1999 <nikamhemangi1999@gmail.com>
2022-03-30 12:41:49 +0530

    video8

---
+++ textutil/views.py
@@ -0,0 +1,37 @@
+#i have creste this file in python
+from django.http import HttpResponse
+from django.shortcuts import render
+
+#code video 6
+
+#def index(request):
+   # return HttpResponse ('''<h1>hello</h1><a href="https://www.amazon.com/shop/sueoverydesigns">django codes</a>''')
+
+# about(request):
+    #return HttpResponse (" about hello harry")
+
+
+def index(request):
+    params = {'name':'hemangi','place':'mars'}
+    return render(request,'index.html',params)
+    #return HttpResponse("home")
+
+def removepunc(request):
+    return HttpResponse("remove punc")
+
+def capfirst(request):
+    return HttpResponse("capitalize first")
+
+def newlineremove(request):
+    return HttpResponse("newlineremove")
+
+def spaceremove(request):
+    return HttpResponse("spaceremove")
+
+def charcount(request):
+    return HttpResponse("charcount")
+
+
+
+
+
