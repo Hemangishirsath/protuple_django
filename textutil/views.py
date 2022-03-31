fe0148611aba0dc48aef49946a64615a90e2d9ee
HemangiShirsath1999 <nikamhemangi1999@gmail.com>
2022-03-31 16:51:46 +0530

    video15

--- textutil/views.py
+++ textutil/views.py
@@ -40,9 +40,50 @@ def ex1(request):
             analyzed = analyzed + char
     params = {'purpose': 'changed to upper', 'analyzed_text': analyzed}
     return render(request, 'analyze.html', params)
-
-
     #params ={'purpose':'remove punctuations','analyzed text':'analyze'}
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    elif (fullcaps == "on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+
+        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'analyze.html', params)
+
+    elif (extraspaceremover == "on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not (djtext[index] == " " and djtext[index + 1] == " "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'analyze.html', params)
+
+    elif (newlineremover == "on"):
+        analyzed = ""
+        for char in djtext:
+            if char != "\n" and char != "\r":
+                analyzed = analyzed + char
+            else:
+                print("no")
+        print("pre", analyzed)
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        print(params)
+        # Analyze the text
+        return render(request, 'analyze.html', params)
+    else:
+        return HttpResponse("Error")
     #return render(request,'analyze.html',params)
     #return HttpResponse("remove punc")
 '''
